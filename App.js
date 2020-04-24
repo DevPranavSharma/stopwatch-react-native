ea2f608f80752a7d493cab40e21d00646308b917
Pranav Sharma <sharmapranavkkp@gmail.com>
2020-04-24 18:42:20 +0530

    added pause/stop functionality

--- App.js
+++ App.js
@@ -7,27 +7,54 @@ const instructions = Platform.select({
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
+var functionInt
 export default class App extends Component {
   constructor(props){
    super(props)
-   this.updateTime=this.updateTime.bind(this)
+   this.updateTime=this.updateTime.bind(this);
+   this.onButtonPress=this.onButtonPress.bind(this)
+   this.onPressStop=this.onPressStop.bind(this)
    this.state={
-     count: 0
+     count: 0,
+     pressed: false
    }
+
+  }
+  onButtonPress(){
+    if(!this.state.pressed){
+      this.setState({pressed:true});
+      this.updateTime()
+    }
   }
   updateTime(){
-    setInterval(()=>{this.setState((state)=>{return {count: this.state.count+1}})},1000)
+   functionInt= setInterval(()=>{this.setState((state)=>{return {count: this.state.count+1}})},1000)
+  }
+  onPressStop(){
+    this.setState({pressed:false});
+    clearInterval(functionInt)
   }
  render(){ return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Stopwatch</Text>
  <Text style={styles.welcome}>{this.state.count}</Text>
- <Button onPress={this.updateTime} title="Start"/>
+ <View style={styles.buttonrow}> 
+   <Button style={styles.btn}onPress={this.onButtonPress} title="Start"/>
+ <Button style={styles.btn} onPress={this.onPressStop} title="Stop"/>
+ </View>
+
     </View>
   );}
 }
 
 const styles = StyleSheet.create({
+  buttonrow:{
+     flexDirection:'row',
+     margin: 5
+  },
+  btn:{
+    paddingRight:5,
+    margin:5
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
